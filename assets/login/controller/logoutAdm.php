c48637a7e4a84bca9882ce9127b75d2f83ae296d
PereiraXZ <jp89gf@gmail.com>
2023-08-06 22:26:27 -0300

    Create | Tela para redefinição de senha (ADM)

--- assets/login/controller/logoutAdm.php
+++ assets/login/controller/logoutAdm.php
@@ -1,9 +1,13 @@
 <?php
 
     session_start();
+
     session_unset();
+
     session_destroy();
+
     setcookie(session_name(), '', time() - 60, '/');
+    
     header('Location: ../../login/view/loginAdm.html');
     exit();
 
